aa5bb94be0d6a3e94584ac0714b2f50237d558db
Andriy Mykhaylyk <erp.lsf@gmail.com>
2025-06-15 16:10:09 +0200

    add conditionals

--- cbits-unix/init.c
+++ cbits-unix/init.c
@@ -8,6 +8,10 @@
 
 uint64_t splitmix_init() {
 	uint64_t result;
+#if (!defined(__ANDROID__) || __ANDROID_API__ >= 28)
 	int r = getentropy(&result, sizeof(uint64_t));
+#else
+	int r = -1;
+#endif
 	return r == 0 ? result : 0xfeed1000;
 }
